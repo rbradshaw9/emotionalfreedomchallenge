b10b91c29234482f6f9dc7a77753371c74ee8e7f
Ryan Bradshaw <ryanbradshaw@Ryans-MacBook-Pro-2.local>
2026-02-17 12:52:10 -0400

    Add /getvip redirect to Infusionsoft VIP order form

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/getvip',
+        destination: 'https://bl843.infusionsoft.com/app/orderForms/cdb82d2b-34ac-443e-aba9-2c63993be424',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
