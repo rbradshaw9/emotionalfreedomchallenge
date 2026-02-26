aed789a6b2b0d3f47fea845dd40abf849ab860fd
Ryan Bradshaw <ryanbradshaw@Ryans-MacBook-Pro-2.local>
2026-02-26 12:09:24 -0400

    fix: update Infusionsoft version and reCAPTCHA script URLs to 1.70.0.906323

--- app/(main)/partners/register/page.tsx
+++ app/(main)/partners/register/page.tsx
@@ -63,7 +63,7 @@ export default function PartnerRegister() {
             >
               <input name="inf_form_xid" type="hidden" value="4c9b8b75fc0b1e19505d18dac0e1a6ab" />
               <input name="inf_form_name" type="hidden" value="Emotional Freedom Challenge&#xa;Referral Sign-up submitted" />
-              <input name="infusionsoft_version" type="hidden" value="1.70.0.905848" />
+              <input name="infusionsoft_version" type="hidden" value="1.70.0.906323" />
 
               <div className="section-title">
                 <span>Part 1 - Contact Information</span>
@@ -179,8 +179,8 @@ export default function PartnerRegister() {
             </form>
 
             <Script src="https://bl843.infusionsoft.app/app/webTracking/getTrackingCode" strategy="afterInteractive" />
-            <Script src="https://bl843.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.905848-hf-202602232108" strategy="afterInteractive" />
-            <Script src="https://bl843.infusionsoft.com/resources/external/recaptcha/production/enterpriseRecaptcha.js?b=1.70.0.905848-hf-202602232108" strategy="afterInteractive" />
+            <Script src="https://bl843.infusionsoft.com/resources/external/recaptcha/production/recaptcha.js?b=1.70.0.906323-hf-202602241301" strategy="afterInteractive" />
+            <Script src="https://bl843.infusionsoft.com/resources/external/recaptcha/production/enterpriseRecaptcha.js?b=1.70.0.906323-hf-202602241301" strategy="afterInteractive" />
             <Script src="https://bl843.infusionsoft.com/app/timezone/timezoneInputJs?xid=4c9b8b75fc0b1e19505d18dac0e1a6ab" strategy="afterInteractive" />
             <Script src="https://bl843.infusionsoft.com/js/jquery/jquery-3.3.1.js" strategy="afterInteractive" />
             <Script src="https://bl843.infusionsoft.app/app/webform/overwriteRefererJs" strategy="afterInteractive" />
